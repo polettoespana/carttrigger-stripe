9a769ea6836f7ad190a9437085b0b8d8bd18442f
polettoespanadevteam <info@poletto.es>
2026-05-23 14:39:33 +0200

    ECE layout: maxRows 0 + overflow auto (removes 'see more' button)

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -75,7 +75,7 @@
         var el    = elems.create( 'expressCheckout', {
             buttonType:   { applePay: 'buy', googlePay: 'buy' },
             buttonHeight: parseInt( ctstripe.ece_height, 10 ) || 44,
-            layout:       { maxColumns: parseInt( ctstripe.ece_columns, 10 ) || 2, maxRows: 0, overflow: 'never' },
+            layout:       { maxColumns: parseInt( ctstripe.ece_columns, 10 ) || 2, maxRows: 0, overflow: 'auto' },
         } );
 
         el.on( 'ready', function ( event ) {
